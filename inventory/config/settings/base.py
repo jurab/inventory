3dc97eaedf029ae95aed659949d1ba658feec722
jurab <brazdil94@gmail.com>
2022-10-25 13:45:21 +0200

    Default DB switch

--- inventory/config/settings/base.py
+++ inventory/config/settings/base.py
@@ -98,11 +98,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'postgres': {
+    'default': {
         'ENGINE': "django.db.backends.postgresql_psycopg2",
         'NAME': os.environ.get('POSTGRES_DBNAME') or "inventory",
         'USER': os.environ.get('POSTGRES_USER') or "jura",
